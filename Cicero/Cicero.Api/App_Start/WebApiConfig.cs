0e204df1f297069061322840bb6f8d34c072d778
jangtrektang <bert.limerkens@gmail.com>
2017-02-18 09:51:55 +0100

    fixed api/token access

--- Cicero/Cicero.Api/App_Start/WebApiConfig.cs
+++ Cicero/Cicero.Api/App_Start/WebApiConfig.cs
@@ -11,8 +11,6 @@ namespace Cicero.Api
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
             // Web API routes
             config.MapHttpAttributeRoutes();
 
